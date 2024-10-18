3889439ae857dec0ff5fd9a363dffd8c34492dbc
felipecremonez <lipecremone@gmail.com>
2024-10-18 12:15:13 -0300

    update

--- src/script.js
+++ src/script.js
@@ -1,3 +1,21 @@
-function editNote() {
-    document.getElementsById('edit')
+
+const localStorageNameKey = "to-do-list"
+
+function newTask(){
+    let input = document.getElementById("inputNewTask");
+    
+    //validação
+    if(input.value){
+        alert("Digite algo para ser inserido na lista.")
+    }
+    // else if()
+    else{
+    //increment to LocalStorage
+    let values = JSON.parse(localStorage.getItem("to-do-list")) || "[]"
+    values.push({
+        name: input.value
+    });
+    localStorage.setItem(localStorageNameKey, JSON.stringify(values));
+    }
+
 }
