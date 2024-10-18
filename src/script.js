ebd369642cef310eea269299893d8c6a60fc9684
felipecremonez <lipecremone@gmail.com>
2024-10-18 17:14:57 -0300

    update

--- src/script.js
+++ src/script.js
@@ -5,17 +5,43 @@ function newTask(){
     let input = document.getElementById("inputNewTask");
     
     //validação
-    if(input.value){
+    if( !input.value){
         alert("Digite algo para ser inserido na lista.")
     }
     // else if()
     else{
     //increment to LocalStorage
-    let values = JSON.parse(localStorage.getItem("to-do-list")) || "[]"
+    let values = JSON.parse(localStorage.getItem("to-do-list")) || []
     values.push({
         name: input.value
     });
-    localStorage.setItem(localStorageNameKey, JSON.stringify(values));
+    localStorage.setItem(localStorageNameKey, JSON.stringify(values))
+    showValues()
+    }
+
+
+
+}
+
+function showValues(){
+    let values = JSON.parse(localStorage.getItem("to-do-list")) || []
+    let list = document.getElementById("to-do-list")
+    list.innerHTML = ''
+    for(let i = 0; i < values.length; i++)
+    {
+        list.innerHTML += `<li>${values[i]['name']} <button id='btn-ok' onclick='removeItem("${values[i].name}")'>Ok</button><li>`
     }
 
 }
+
+
+function removeItem(data){
+    let values = JSON.parse(localStorage.getItem("to-do-list")) || []
+    let index = values.findIndex( x => x.name == data)
+    values.splice(index, 1)
+    localStorage.setItem(localStorageNameKey, JSON.stringify(values))
+    showValues()
+}
+
+showValues()
+
