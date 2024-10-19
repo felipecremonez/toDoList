99038dc01174d2d612060c04d65740231e8daaff
felipecremonez <lipecremone@gmail.com>
2024-10-19 10:28:54 -0300

    update

--- src/script.js
+++ src/script.js
@@ -1,14 +1,23 @@
 
 const localStorageNameKey = "to-do-list"
 
+function validateIfExistsNewTask(){
+    let values = JSON.parse(localStorage.getItem(localStorageNameKey) || [])
+    let inputValue = document.getElementById("inputNewTask").value
+    let exists = values.find(x => x.name == inputValue)
+    return !exists ? false : true 
+}
 function newTask(){
     let input = document.getElementById("inputNewTask");
-    
+    input.style.border = ''
     //validação
     if( !input.value){
+        input.style.border = "2px solid red"
         alert("Digite algo para ser inserido na lista.")
     }
-    // else if()
+    else if(validateIfExistsNewTask()){
+        alert("Tarefa ja existe na lista.")
+    }
     else{
     //increment to LocalStorage
     let values = JSON.parse(localStorage.getItem("to-do-list")) || []
@@ -19,6 +28,7 @@ function newTask(){
     showValues()
     }
 
+    input.value = ''
 
 
 }
@@ -29,7 +39,9 @@ function showValues(){
     list.innerHTML = ''
     for(let i = 0; i < values.length; i++)
     {
-        list.innerHTML += `<li>${values[i]['name']} <button id='btn-ok' onclick='removeItem("${values[i].name}")'>Ok</button><li>`
+        list.innerHTML += `<li>${values[i]['name']} <button id='btn-ok' onclick='removeItem("${values[i].name}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+  <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+</svg></button><li>`
     }
 
 }
